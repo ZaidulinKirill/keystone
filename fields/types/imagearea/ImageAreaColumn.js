471f8fb28417cc28d3cbce3f85b56d021e96b434
Кирилл Зайдулин <zaidulinkirill@gmail.com>
2018-12-11 18:28:19 +0300

    Test ImageArea as CloudinaryImage

--- fields/types/imagearea/ImageAreaColumn.js
+++ fields/types/imagearea/ImageAreaColumn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import numeral from 'numeral';
+import CloudinaryImageSummary from '../../components/columns/CloudinaryImageSummary';
 import ItemsTableCell from '../../components/ItemsTableCell';
 import ItemsTableValue from '../../components/ItemsTableValue';
 
@@ -9,23 +9,25 @@ var ImageAreaColumn = React.createClass({
 		col: React.PropTypes.object,
 		data: React.PropTypes.object,
 	},
-	renderValue () {
-		const value = this.props.data.fields[this.props.col.path];
-		if (value === undefined || isNaN(value)) return null;
+	renderValue: function () {
+		var value = this.props.data.fields[this.props.col.path];
+		if (!value || !Object.keys(value).length) return;
 
-		const formattedValue = (this.props.col.path === 'money') ? numeral(value).format('$0,0.00') : value;
+		return (
+			<ItemsTableValue field={this.props.col.type}>
+				<CloudinaryImageSummary label="dimensions" image={value} secure={this.props.col.field.secure} />
+			</ItemsTableValue>
+		);
 
-		return formattedValue;
 	},
 	render () {
 		return (
 			<ItemsTableCell>
-				<ItemsTableValue field={this.props.col.type}>
-					{this.renderValue()}
-				</ItemsTableValue>
+				{this.renderValue()}
 			</ItemsTableCell>
 		);
 	},
 });
 
 module.exports = ImageAreaColumn;
+
--- fields/types/imagearea/ImageAreaFilter.js
+++ fields/types/imagearea/ImageAreaFilter.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import { findDOMNode } from 'react-dom';
-import {
-	Form,
-	FormField,
-	FormInput,
-	FormSelect,
-	Grid,
-} from '../../../admin/client/App/elemental';
 
-const MODE_OPTIONS = [
-	{ label: 'Exactly', value: 'equals' },
-	{ label: 'Greater Than', value: 'gt' },
-	{ label: 'Less Than', value: 'lt' },
-	{ label: 'Between', value: 'between' },
+import { SegmentedControl } from '../../../admin/client/App/elemental';
+
+const OPTIONS = [
+	{ label: 'Is Set', value: true },
+	{ label: 'Is NOT Set', value: false },
 ];
 
 function getDefaultValue () {
 	return {
-		mode: MODE_OPTIONS[0].value,
-		value: '',
+		exists: true,
 	};
 }
 
 var ImageAreaFilter = React.createClass({
+	propTypes: {
+		filter: React.PropTypes.shape({
+			exists: React.PropTypes.oneOf(OPTIONS.map(i => i.value)),
+		}),
+	},
 	statics: {
 		getDefaultValue: getDefaultValue,
 	},
@@ -31,115 +27,22 @@ var ImageAreaFilter = React.createClass({
 			filter: getDefaultValue(),
 		};
 	},
-
-	componentDidMount () {
-		// focus the text input
-		findDOMNode(this.refs.focusTarget).focus();
+	toggleExists (value) {
+		this.props.onChange({ exists: value });
 	},
-
-	handleChangeBuilder (type) {
-		const self = this;
-		return function handleChange (e) {
-			const { filter, onChange } = self.props;
-
-			switch (type) {
-				case 'minValue':
-					onChange({
-						mode: filter.mode,
-						value: {
-							min: e.target.value,
-							max: filter.value.max,
-						},
-					});
-					break;
-				case 'maxValue':
-					onChange({
-						mode: filter.mode,
-						value: {
-							min: filter.value.min,
-							max: e.target.value,
-						},
-					});
-					break;
-				case 'value':
-					onChange({
-						mode: filter.mode,
-						value: e.target.value,
-					});
-			}
-		};
-	},
-	// Update the props with this.props.onChange
-	updateFilter (changedProp) {
-		this.props.onChange({ ...this.props.filter, ...changedProp });
-	},
-	// Update the filter mode
-	selectMode (e) {
-		this.updateFilter({ mode: e.target.value });
-
-		// focus on next tick
-		setTimeout(() => {
-			findDOMNode(this.refs.focusTarget).focus();
-		}, 0);
-	},
-
-	renderControls (mode) {
-		let controls;
-		const { field } = this.props;
-		const placeholder = field.label + ' is ' + mode.label.toLowerCase() + '...';
-
-		if (mode.value === 'between') {
-			controls = (
-				<Grid.Row xsmall="one-half" gutter={10}>
-					<Grid.Col>
-						<FormInput
-							onChange={this.handleChangeBuilder('minValue')}
-							placeholder="Min."
-							ref="focusTarget"
-							type="number"
-						/>
-					</Grid.Col>
-					<Grid.Col>
-						<FormInput
-							onChange={this.handleChangeBuilder('maxValue')}
-							placeholder="Max."
-							type="number"
-						/>
-					</Grid.Col>
-				</Grid.Row>
-			);
-		} else {
-			controls = (
-				<FormInput
-					onChange={this.handleChangeBuilder('value')}
-					placeholder={placeholder}
-					ref="focusTarget"
-					type="number"
-				/>
-			);
-		}
-
-		return controls;
-	},
-
 	render () {
 		const { filter } = this.props;
-		const mode = MODE_OPTIONS.filter(i => i.value === filter.mode)[0];
 
 		return (
-			<Form component="div">
-				<FormField>
-					<FormSelect
-						onChange={this.selectMode}
-						options={MODE_OPTIONS}
-						value={mode.value}
-					/>
-				</FormField>
-				{this.renderControls(mode)}
-			</Form>
+			<SegmentedControl
+				equalWidthSegments
+				onChange={this.toggleExists}
+				options={OPTIONS}
+				value={filter.exists}
+			/>
 		);
 	},
-
 });
 
 module.exports = ImageAreaFilter;
+
