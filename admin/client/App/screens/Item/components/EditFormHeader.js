cc42eaa71831b1a832d12633c66f14318cae2608
ZaidulinKirill <zaidulinkirill@gmail.com>
2019-03-12 12:47:28 +0300

    Fix

--- admin/client/App/screens/Item/components/EditFormHeader.js
+++ admin/client/App/screens/Item/components/EditFormHeader.js
@@ -47,10 +47,10 @@ export const EditFormHeader = React.createClass({
 		);
 	},
 	renderDrilldownItems () {
-		const user = JSON.parse(Cookies.get('user') || '{}');
-		if (user && user.isAuthor) {
-			return null;
-		}
+		// const user = JSON.parse(Cookies.get('user') || '{}');
+		// if (user && user.isAuthor) {
+		// 	return null;
+		// }
 
 		const { data, list } = this.props;
 		const items = data.drilldown ? data.drilldown.items : [];
