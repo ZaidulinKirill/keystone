8b235769ffd3339d3a72a596c67f5cbf1d9fecb9
ZaidulinKirill <zaidulinkirill@gmail.com>
2019-03-15 19:15:34 +0300

    Fix

--- admin/client/App/components/Navigation/Mobile/index.js
+++ admin/client/App/components/Navigation/Mobile/index.js
@@ -74,8 +74,11 @@ const MobileNavigation = React.createClass({
 		const user = JSON.parse(Cookies.get('user') || '{}');
 
 		return this.props.sections
-		.filter(section => !user || !user.isAuthor || (section.label === 'Works / Работы' || section.label === 'Authors / Авторы'
-			|| section.label === 'Work Application / Регистрация работ'))
+		.filter(section => {
+			console.log(section);
+			return !user || !user.isAuthor || (section.label === 'Works / Работы' || section.label === 'Authors / Авторы'
+			|| section.label === 'Work Application / Регистрация работ');
+		})
 		.map((section) => {
 			// Get the link and the classname
 			const href = section.lists[0].external ? section.lists[0].path : `${Keystone.adminPath}/${section.lists[0].path}`;
