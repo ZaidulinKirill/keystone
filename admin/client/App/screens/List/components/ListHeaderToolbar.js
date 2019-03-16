cf85018613316ede372197b0597767c08cdd82f1
ZaidulinKirill <zaidulinkirill@gmail.com>
2019-03-16 12:33:02 +0300

    Fix

--- admin/client/App/screens/List/components/ListHeaderToolbar.js
+++ admin/client/App/screens/List/components/ListHeaderToolbar.js
@@ -25,14 +25,10 @@ function ButtonDivider ({ style, ...props }) {
 };
 
 function CreateButton ({ listName, onClick, ...props }) {
-	console.log(listName);
-
 	const user = JSON.parse(Cookies.get('user') || '{}');
-	if (user && user.isAuthor && listName !== 'Work Application / Регистрация работ') {
+	if (user && user.isAuthor && listName !== 'Work Application') {
 		return null;
 	}
-
-	console.log('e' + listName);
 
 	return (
 		<GlyphButton
